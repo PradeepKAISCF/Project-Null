1cbac178e124320d272fe6cbc1dde3884b9b0bba
PradeepKAISCF <129294519+PradeepKAISCF@users.noreply.github.com>
2023-10-29 14:33:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,11 +10,7 @@ const app = express();
 dotenv.config()
 app.use(express.json({limit:'30mb',extended: true}))
 app.use(express.urlencoded({limit:"30mb", extended: true}))
-/*app.use(cors({
-    origin: "https://project-null-frontend.vercel.app/",
-    methods: ["POST","GET","PATCH"],
-    credentials: true
-}));*/
+app.use(cors());
 
 app.get('/',(req,res) => {
     res.send("This is a stack overflow clone API")
