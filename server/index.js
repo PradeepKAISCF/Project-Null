5af2493a8fdf661dc8419c853b608a4fa2765e5e
PradeepKAISCF <129294519+PradeepKAISCF@users.noreply.github.com>
2023-10-29 22:50:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,14 +10,9 @@ const app = express();
 dotenv.config()
 app.use(express.json({limit:'30mb',extended: true}))
 app.use(express.urlencoded({limit:"30mb", extended: true}))
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+
+app.use(cors())
 
 app.get('/',(req,res) => {
     res.send("This is a stack overflow clone API")
