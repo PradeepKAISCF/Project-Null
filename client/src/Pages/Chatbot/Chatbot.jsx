bf4a6c2107fe32f5c507b68122becca1ef161b81
PradeepKAISCF <129294519+PradeepKAISCF@users.noreply.github.com>
2024-01-31 23:41:20 +0530

    Update Chatbot.jsx

--- client/src/Pages/Chatbot/Chatbot.jsx
+++ client/src/Pages/Chatbot/Chatbot.jsx
@@ -6,7 +6,7 @@ import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
