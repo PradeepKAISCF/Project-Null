ffdbe2c011829d96ed175c6dd8bc2d0412264c79
PradeepKAISCF <129294519+PradeepKAISCF@users.noreply.github.com>
2024-02-01 22:21:53 +0530

    Update Chatbot.jsx

--- client/src/Pages/Chatbot/Chatbot.jsx
+++ client/src/Pages/Chatbot/Chatbot.jsx
@@ -6,7 +6,7 @@ import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "sk-YPW7wFT1c6nlP7Jaow5MT3BlbkFJPyOUT71N5Ttoh7KbfmK";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
@@ -91,7 +91,7 @@ const Chatbot = () => {
           const response = await fetch("https://api.openai.com/v1/chat/completions", {
             method: "POST",
             headers: {
-              "Authorization": `Bearer  ${API_KEY}`,
+              "Authorization": `Bearer  ${API_KEY}1`,
               "Content-Type": "application/json"
             },
             body: JSON.stringify(apiRequestBody)
